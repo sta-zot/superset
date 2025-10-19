81203a4883842f021696a00cc4453d595ac77460
Stanislav Zotov <zss@ya.ru>
2025-10-19 13:58:51 +0300

    Replaced view template

--- superset/custom_form/view.py
+++ superset/custom_form/view.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+`from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_appbuilder import SimpleFormView, expose, BaseView
 from superset.superset_typing import FlaskResponse
 from superset.views.base import BaseSupersetView
@@ -13,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 logger.error("Custom view module loaded")
 
 
-class ReportUploadView(BaseView):
+class ReportUploadView(BaseSupersetView):
     form = None
     choices = [
             ('1', 'Мероприятия по ПФГ для экономически активного населения и пенсионеров'),
@@ -43,17 +43,3 @@ class ReportUploadView(BaseView):
         ]
         self.form.activity_field.choices = self.choices
         return self.render_template("upload_report.html", form=self.form)
-
-
-    # def form_get(self, form: ReportUploadForm) -> None:
-    #     form.actyvity_field.data = '1'
-
-    # def form_post(self, form: ReportUploadForm) -> FlaskResponse:
-    #     # Логика обработки загруженного файла и других данных формы
-    #     report_file = request.files[form.report.name]
-    #     actyvity_field = form.actyvity_field.data
-    #     # Здесь можно добавить код для сохранения файла в объектное хранилище
-    #     flash(self.message, self.message_category)
-    #     flash(report_file.filename, self.message_category)
-    #     flash(actyvity_field, self.message_category)
-    #     return redirect(url_for("ReportUploadView.form"))
