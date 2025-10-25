434d2d28f44bad258bff3b58ad690ea34f7bc196
Stanislav Zotov <zss@ya.ru>
2025-10-25 22:46:48 +0300

    Отключён импорт плагина

--- superset/plugins/report_upload_form/__init__.py
+++ superset/plugins/report_upload_form/__init__.py
@@ -2,20 +2,13 @@
 В этом файле можно определить пользовательские формы для Superset.
 Формы предназначены для загрузки отчетов в объектное хранилище.
 """
-from .model import (
-    LocationsModel,
-    ObjectStorage,
-    DocumentStorage
-) # noqa: F401
-from .forms import ReportUploadForm # noqa: F401
+# from .model import (
+#     LocationsModel,
+#     ObjectStorage,
+#     DocumentStorage
+# ) # noqa: F401
+# from .forms import ReportUploadForm # noqa: F401
 from .view import ReportUploadView # noqa: F401
-
-
 import logging
 
 logging.warning("Custom form module initialized")
-
-
-
-
-
--- superset/plugins/report_upload_form/view.py
+++ superset/plugins/report_upload_form/view.py
@@ -13,11 +13,11 @@ from superset.superset_typing import FlaskResponse
 # from superset.views.base import BaseSupersetView
 
 from .forms import ReportUploadForm
-from .model import (
-    LocationsModel,
-    ObjectStorage,
-    DocumentStorage
-)
+from .model import Locations #(
+#   Locations,
+#    ObjectStorage,
+#    DocumentStorage
+#)
 import logging
 
 
@@ -47,7 +47,7 @@ class ReportUploadView(BaseView):
     def form_view(self) -> FlaskResponse:
         if not self.form:
             self.form = ReportUploadForm()
-        locations = LocationsModel()
+        locations = Locations()
         regions = locations.get_regions()
         self.form.region_field.choices = [
             (regions.index(region), region)
@@ -56,96 +56,96 @@ class ReportUploadView(BaseView):
         self.form.activity_field.choices = self.activities
         return self.render_template("upload_report.html", form=self.form)
 
-    @expose("/upload", methods=["POST"])
-    def upload(self) -> FlaskResponse:
-        if "file" not in request.files:
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Не выбран файл"
-                }
-            ), 400  
-        file = request.files["report_field"]
-        if file.filename == "":
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Не выбран файл"
-                }
-            ), 400
+    # @expose("/upload", methods=["POST"])
+    # def upload(self) -> FlaskResponse:
+    #     if "file" not in request.files:
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Не выбран файл"
+    #             }
+    #         ), 400  
+    #     file = request.files["report_field"]
+    #     if file.filename == "":
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Не выбран файл"
+    #             }
+    #         ), 400
 
-        if request.form.get("region_field") == "":
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Не выбран регион"
-                }
-            ), 400
-        else:
-            region = request.form.get("region_field")
+    #     if request.form.get("region_field") == "":
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Не выбран регион"
+    #             }
+    #         ), 400
+    #     else:
+    #         region = request.form.get("region_field")
 
-        if request.form.get("activity_field") == "":
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Не выбрано направление деятельности"
-                }
-            ), 400
-        else:
-            activity = request.form.get("activity_field")
+    #     if request.form.get("activity_field") == "":
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Не выбрано направление деятельности"
+    #             }
+    #         ), 400
+    #     else:
+    #         activity = request.form.get("activity_field")
 
-        comment = request.form.get("comment_field") 
+    #     comment = request.form.get("comment_field") 
 
-        s3 = ObjectStorage(
-            s3_access_key=app.config["S3_ACCESS_KEY"],
-            s3_secret_key=app.config["S3_SECRET_KEY"],
-            bucket_name=app.config["S3_BUCKET"],
-            endpoint_url=app.config["S3_ENDPOINT_URL"]
-        )
-        ds = DocumentStorage(
-            host=app.config["DS_HOST"],
-            port=app.config["DS_PORT"],
-            username=app.config["DS_USER"],
-            password=app.config["DS_PASSWD"],
-            database=app.config["DS_DB"]
-        )
+    #     s3 = ObjectStorage(
+    #         s3_access_key=app.config["S3_ACCESS_KEY"],
+    #         s3_secret_key=app.config["S3_SECRET_KEY"],
+    #         bucket_name=app.config["S3_BUCKET"],
+    #         endpoint_url=app.config["S3_ENDPOINT_URL"]
+    #     )
+    #     ds = DocumentStorage(
+    #         host=app.config["DS_HOST"],
+    #         port=app.config["DS_PORT"],
+    #         username=app.config["DS_USER"],
+    #         password=app.config["DS_PASSWD"],
+    #         database=app.config["DS_DB"]
+    #     )
 
-        try:
-            s3.add(file, prefix=activity.title())
-        except Exception as e:
-            logger.error(f"Error uploading file to S3: {e}")
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Ошибка загрузки файла"
-                }
-            ), 500
-        try:
-            ds.add(
-                {
-                    "author": g.user,
-                    "region": region,
-                    "activity": {
-                        "id": activity.title(),
-                        "title": self.activities[activity.title()]
-                        },
-                    "created_at": datetime.now().timestamp(),
-                    "comment": comment,
-                    "prefix": activity.title(),
-                    "filename": file.filename,
-                }
-            )
-        except Exception as e:
-            logger.error(f"Error adding report to DB: {e}")
-            return jsonfy(
-                {
-                    "status": "Error",
-                    "message": "Ошибка загрузки файла"
-                }
-            ), 500
-        return jsonfy(
-            {
-                "status": "Success",
-                "message": "Файл успешно загружен"
-            }
-        ), 200
+    #     try:
+    #         s3.add(file, prefix=activity.title())
+    #     except Exception as e:
+    #         logger.error(f"Error uploading file to S3: {e}")
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Ошибка загрузки файла"
+    #             }
+    #         ), 500
+    #     try:
+    #         ds.add(
+    #             {
+    #                 "author": g.user,
+    #                 "region": region,
+    #                 "activity": {
+    #                     "id": activity.title(),
+    #                     "title": self.activities[activity.title()]
+    #                     },
+    #                 "created_at": datetime.now().timestamp(),
+    #                 "comment": comment,
+    #                 "prefix": activity.title(),
+    #                 "filename": file.filename,
+    #             }
+    #         )
+    #     except Exception as e:
+    #         logger.error(f"Error adding report to DB: {e}")
+    #         return jsonfy(
+    #             {
+    #                 "status": "Error",
+    #                 "message": "Ошибка загрузки файла"
+    #             }
+    #         ), 500
+    #     return jsonfy(
+    #         {
+    #             "status": "Success",
+    #             "message": "Файл успешно загружен"
+    #         }
+    #     ), 200
